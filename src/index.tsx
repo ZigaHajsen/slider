0f18495faaaa8556d7fb8a962f17361f99dbba6c
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-11-23 18:21:50 +0100

    Initial setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './components';
+import { GlobalStyles } from './GlobalStyles';
 
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <GlobalStyles />
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
